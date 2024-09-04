c91b7de3967f6b3a643d4546004882637ec9f094
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-09-04 22:44:04 +0530

    first commit

---
+++ controllers/taskController.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Task = require('../models/Task');
+
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createTask = async (req, res) => {
+  const { title, description, status } = req.body;
+  try {
+    const newTask = new Task({ title, description, status });
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, status } = req.body;
+  try {
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.status = status || task.status;
+    await task.save();
+    res.json(task);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid task ID' });
+    }
+
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    await Task.deleteOne({ _id: id });
+    res.json({ message: 'Task removed' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getTasks, createTask, updateTask, deleteTask };

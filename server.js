6041c0e29d5876cfa0fe6fa025d425ad540f6cd4
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-09-04 23:01:35 +0530

    second commit

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+  });
+  
 app.use('/api/tasks', taskRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
